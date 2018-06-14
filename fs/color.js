e9b5c116545e7f9d6950a4ce2e138ba89ed8e166
dude-mansaa <39621383+dude-mansaa@users.noreply.github.com>
2018-06-14 13:57:20 +0530

    Update color.js

--- fs/color.js
+++ fs/color.js
@@ -33,7 +33,7 @@ let color = function(args){
 			DR = r/255;
 			DG = g/255;
 			DB = b/255;
-			DW = w/127;
+			DW = w/200;
 			PWM.set(red,1000,DR);
 			PWM.set(green,1000,DG);
 			PWM.set(blue,1000,DB);
@@ -53,9 +53,9 @@ let color = function(args){
 			PWM.set(white,1000,DW);
 			return 'success'
 		}else{
-			DR = r/127;
-			DG = g/127;
-			DB = b/127;
+			DR = r/200;
+			DG = g/200;
+			DB = b/200;
 			DW = w/255;
 			PWM.set(red,1000,DR);
 			PWM.set(green,1000,DG);
