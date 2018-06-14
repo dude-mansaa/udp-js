1af5d2b282721438dccc02e45314ea28cf41c091
dude-mansaa <39621383+dude-mansaa@users.noreply.github.com>
2018-06-14 12:56:38 +0530

    Update color.js

--- fs/color.js
+++ fs/color.js
@@ -1,6 +1,5 @@
-load('api_pwm.js');
+load(api_pwm.js);
 
-let floor = ffi('double floor(double)');
 let red = 4;
 let green = 15;
 let blue = 5;
@@ -11,52 +10,43 @@ GPIO.set_mode(blue,GPIO.MODE_OUTPUT);
 GPIO.set_mode(white,GPIO.MODE_OUTPUT);
 
 let color = function(args){
-  let r,g,b=0;
+	let r,g,b,w=0;
 	let model = args.p;
-	let hi = floor(args.h/60);
-	let F = args.h-hi;
-	let P = args.v * (1-args.s);
-	let Q = args.v * (1 -F * args.s);
-	let T = args.v * (1 - args.s * (1-F));
-	if(hi === 0){
-		r = args.v;
-	    	g = T;
-	    	b = P;
-	}else if(hi === 1){
-		r = Q;
-	    	g = args.v;
-	    	b = P;
-	}else if(hi === 2){
-		r = P;
-	    	g = args.v;
-	    	b = T;
-	}else if(hi === 3){
-		r = P;
-	   	g = Q;
-	   	b = args.v;
-	}else if(hi === 4){
-		r = T;
-	    	g = P;
-	    	b = args.v;
-	}else if(hi === 5){
-		r = args.v;
-	    	g = P;
-	    	b = Q;
-	}	
-	let DR = r*255/100;
-	let DG = g*255/100;
-	let DB = b*255/100;
-	let DW = args.s;
+	r = args.r;
+	g = args.g;
+	b = args.b;
+	w = args.w;
+	let DR,DG,DB,DW=0;
 	if(model === 'rgb'){
-	  PWM.set(red,1000,DR);
-	  PWM.set(green,1000,DG);
-	  PWM.set(blue,1000,DB);
-	  PWM.set(white,1000,DW*50/100);
+		if(r > 0 && g > 0 && b > 0){
+			let avg = r+g+b/3
+			DR = 0;
+			DG = 0;
+			DB = 0;
+			DW = avg*255/100;
+		}else{
+			DR = r*255/100;
+			DG = g*255/100;
+			DB = b*255/100;
+			DW = w*127/100;
+		}
 	}else if(model === 'ct'){
-	  PWM.set(red,1000,DR*50/100);
-	  PWM.set(green,1000,DG*50/100);
-	  PWM.set(blue,1000,DB*50/100);
-	  PWM.set(white,1000,DW);
+		if(r > 0 && g > 0 && b > 0){
+			let avg = r+g+b/3
+			DR = 0;
+			DG = 0;
+			DB = 0;
+			DW = avg*255/100;
+		}else{
+			DR = r*127/100;
+			DG = g*127/100;
+			DB = b*127/100;
+			DW = w*255/100;
+		}
 	}
+	PWM.set(red,1000,DR);
+	PWM.set(green,1000,DG);
+	PWM.set(blue,1000,DB);
+	PWM.set(white,1000,DW);
 	return 'success'
 };
